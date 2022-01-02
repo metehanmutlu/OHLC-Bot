fb0636f87da579b61086000999ee44322eb4acd6
mthanmutlu <mthanmutlu44@gmail.com>
2022-01-02 16:13:01 +0300

    ohlc-bot

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const readLastData = require('./modules/readLastData');
 const app = express();
 
 
-app.use('/', (req, res) => {
-    res.send('Use With `https://ohlc-tw-bot.herokuapp.com/api/data/?symbol=UNISWAP:HEXUSDT&period=1D`')
-})
-
 app.get('/api/data/', (req, res) => {
     const queries = req.query;
     if (queries.hasOwnProperty('symbol') && queries.hasOwnProperty('period')) {
@@ -21,6 +17,11 @@ app.get('/api/data/', (req, res) => {
         }, 2000);
     }
 });
+
+app.use('/', (req, res) => {
+    res.send('Use With `https://ohlc-tw-bot.herokuapp.com/api/data/?symbol=UNISWAP:HEXUSDT&period=1D`')
+})
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000');
